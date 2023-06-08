275b20f06378977f9014e48db77dfe1adaac90e8
Matt Layman <matthewlayman@gmail.com>
2023-06-07 21:51:34 -0400

    Add template configuration and email confirmation template.
    Fixes #2

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
