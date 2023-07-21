bd2c294b356239e82bf402f4271c151677e60669
Matt Layman <matthewlayman@gmail.com>
2023-07-21 15:02:01 -0400

    Add more of the email sending code.
    For #4

--- journal/entries/jobs/send_mail.py
+++ journal/entries/jobs/send_mail.py
@@ -1,8 +1,11 @@
 from django.core import mail
+from django.template.loader import render_to_string
 from django.utils import timezone
 from django_extensions.management.jobs import DailyJob
 
 from journal.accounts.models import Account
+
+from ..models import Entry
 
 
 class Job(DailyJob):
@@ -12,10 +15,15 @@ class Job(DailyJob):
         accounts = Account.objects.active().select_related("user")
         today = timezone.localdate()
         for account in accounts:
+            # TODO: get *random* entry
+            entry = Entry.objects.filter(user=account.user).last()
+            context = {"entry": entry}
+            text_message = render_to_string("entries/email/prompt.txt", context)
+            html_message = render_to_string("entries/email/prompt.html", context)
             mail.send_mail(
                 subject=f"It's {today:%A}, {today:%b}. {today:%-d}, how are you?",
-                message="Replace this message",
-                html_message="Replace this HTML message",
+                message=text_message,
+                html_message=html_message,
                 from_email="who is this from email",
                 recipient_list=[account.user.email],
             )
--- journal/entries/tests/test_jobs.py
+++ journal/entries/tests/test_jobs.py
@@ -2,6 +2,7 @@ import time_machine
 
 from journal.accounts.tests.factories import UserFactory
 from journal.entries.jobs.send_mail import Job as SendMailJob
+from journal.entries.tests.factories import EntryFactory
 
 
 class TestSendMailJob:
@@ -9,14 +10,30 @@ class TestSendMailJob:
     def test_send_email(self, mailoutbox):
         """An active account receives an email prompt."""
         user = UserFactory()
+        body = "This is the entry.\n\nIt has newlines."
+        entry = EntryFactory(user=user, body=body)
         job = SendMailJob()
 
         job.execute()
 
         assert len(mailoutbox) == 1
         mail = mailoutbox[0]
+        # TODO: assert from_email
         assert mail.to == [user.email]
         assert mail.subject == "It's Wednesday, Jul. 19, how are you?"
-        # TODO: assert message
-        # TODO: assert html_message
-        # TODO: assert from_email
+        assert entry.body in mail.body  # Test the text email.
+        html_message = mail.alternatives[0][0]
+        assert "<p>This is the entry.</p>\n\n<p>It has newlines.</p>" in html_message
+
+    def test_no_available_entries(self, mailoutbox):
+        """The message indicates that a previous entry will appear once it exists."""
+        UserFactory()
+        job = SendMailJob()
+
+        job.execute()
+
+        assert len(mailoutbox) == 1
+        mail = mailoutbox[0]
+        assert "You have no entries yet!" in mail.body  # Test the text email.
+        html_message = mail.alternatives[0][0]
+        assert "<p>You have no entries yet!" in html_message
