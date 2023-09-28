efb1a883beac49c5a881a8d1ae128647b31365ad
Matt Layman <matthewlayman@gmail.com>
2023-09-27 23:36:03 -0400

    Print request headers.

--- journal/core/views.py
+++ journal/core/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
     """The entry point for the website."""
+    for header in request.headers.items():
+        print(header)
     context = {}
     return render(request, "core/index.html", context)
