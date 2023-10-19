5bdb94d3b202f1effeccf63cc475c967d4c3a910
Matt Layman <matthewlayman@gmail.com>
2023-10-18 22:14:15 -0400

    Add some email message logging for send and receive.

--- journal/entries/jobs/send_mail.py
+++ journal/entries/jobs/send_mail.py
@@ -1,5 +1,5 @@
+from anymail.message import AnymailMessage
 from django.conf import settings
-from django.core import mail
 from django.template.loader import render_to_string
 from django.utils import timezone
 from django_extensions.management.jobs import DailyJob
@@ -13,6 +13,7 @@ class Job(DailyJob):
     help = "Sent mail to active accounts"
 
     def execute(self):
+        print("Sending prompts to active accounts")
         accounts = Account.objects.active().select_related("user")
         today = timezone.localdate()
         for account in accounts:
@@ -22,10 +23,18 @@ class Job(DailyJob):
             }
             text_message = render_to_string("entries/email/prompt.txt", context)
             html_message = render_to_string("entries/email/prompt.html", context)
-            mail.send_mail(
-                subject=f"It's {today:%A}, {today:%b}. {today:%-d}, how are you?",
-                message=text_message,
-                html_message=html_message,
+            message = AnymailMessage(
+                subject=f"It's {today:%A}, {today:%b}. {today:%-d}. How are you?",
+                body=text_message,
                 from_email=settings.EMAIL_SENDGRID_REPLY_TO,
-                recipient_list=[account.user.email],
+                to=[account.user.email],
             )
+            message.attach_alternative(html_message, "text/html")
+            message.metadata = {
+                "metadata_key": "metadata_value",
+                "entry_date": str(today),
+            }
+            print(message.metadata)
+            message.send()
+            print("Message ID was:")
+            print(message.anymail_status.message_id)
--- journal/entries/receivers.py
+++ journal/entries/receivers.py
@@ -1,6 +1,33 @@
-def handle_inbound(sender, event, esp_name, **kwargs):
+from pprint import pprint
+from typing import Any
+
+from anymail.signals import AnymailInboundEvent
+
+
+def handle_inbound(
+    sender: Any, event: AnymailInboundEvent, esp_name: str, **kwargs: Any
+) -> None:
     message = event.message
-    print(
-        "Received message from %s (envelope sender %s) with subject '%s'"
-        % (message.from_email, message.envelope_sender, message.subject)
-    )
+    if message is not None:
+        # body - come from the message -
+        # what field is it stored in? what is the format of that field?
+        # message.text OR message.html OR maybe message.stripped_text
+        print("message.text was")
+        print(message.text)
+        print("message.html was")
+        print(message.html)
+        print("message.stripped_text was")
+        print(message.stripped_text)
+
+        # Check event.esp_event
+        print("ESP event was")
+        pprint(event.esp_event)
+
+        # when - It's Wednesday, Oct. 18, how are you? (2023-10-18)
+        # user -
+        # Option 1 - event.from_email - does this match some User in the db?
+        # Option 2 - when the prompt email is sent, SendGrid gives a mail ID.
+        # print(
+        #     "Received message from %s (envelope sender %s) with subject '%s'"
+        #     % (message.from_email, message.envelope_sender, message.subject)
+        # )
