aebbbdbe222aacb9c395e2a856d83946b4da06e0
Matt Layman <matthewlayman@gmail.com>
2023-11-15 22:58:32 -0500

    Add receiver error state logging.
    For #5

--- journal/entries/receivers.py
+++ journal/entries/receivers.py
@@ -13,6 +13,7 @@ def handle_inbound(
 ) -> None:
     message = event.message
     if message is None:
+        print("No message")
         return None
 
     # TODO: parse message.text to remove prompt text
@@ -20,16 +21,19 @@ def handle_inbound(
     try:
         entry_datetime = parse(message.subject, fuzzy_with_tokens=True)[0]  # type: ignore
     except ParserError:
+        print("Bad date parse")
         return None
 
     username = message.from_email.username
     if "." not in username:
+        print("Username missing period.")
         return None
 
     account_id = username.split(".")[1]
     try:
         account = Account.objects.active().get(id=account_id)
     except Account.DoesNotExist:
+        print(f"No active account ID: {account_id}")
         return None
 
     Entry.objects.create(
