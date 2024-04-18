9df0939ea8a29221a42ee8ff72d3aeeb24e0a500
Matt Layman <matthewlayman@gmail.com>
2024-04-17 22:28:52 -0400

    Origin/dependabot/pip/django allauth 0.61.1 (#128)
    * Bump django-allauth from 0.54.0 to 0.61.1
    
    Bumps [django-allauth](https://github.com/pennersr/django-allauth) from 0.54.0 to 0.61.1.
    - [Changelog](https://github.com/pennersr/django-allauth/blob/main/ChangeLog.rst)
    - [Commits](https://github.com/pennersr/django-allauth/compare/0.54.0...0.61.1)
    
    ---
    updated-dependencies:
    - dependency-name: django-allauth
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add allauth middleware.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- project/settings.py
+++ project/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
     "waffle.middleware.WaffleMiddleware",
 ]
 
