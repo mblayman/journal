017e7b1be9095cde0473027d1b485adc7364125e
Matt Layman <matthewlayman@gmail.com>
2024-05-22 21:42:08 -0400

    Add a Prompt admin page.
    Fixes #79

--- journal/entries/admin.py
+++ journal/entries/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import Entry
+from .models import Entry, Prompt
 
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "when"]
+    list_filter = ["when"]
+    raw_id_fields = ["user"]
+
+
+@admin.register(Prompt)
+class PromptAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "when"]
     list_filter = ["when"]
     raw_id_fields = ["user"]
