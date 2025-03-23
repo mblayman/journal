6fcdea36316a2e7b549c1484c64e8bcef844a69c
Matt Layman <matthewlayman@gmail.com>
2025-03-23 15:22:50 -0400

    Remove more Python code.

--- journal/core/tests/test_views.py
+++ journal/core/tests/test_views.py
@@ -1,19 +1,9 @@
 from django.urls import reverse
 
-from journal.accounts.tests.factories import UserFactory
-
 
 class TestIndex:
     def test_unauthenticated(self, client):
         """An unauthenticated user gets a valid response."""
-        response = client.get(reverse("index"))
-
-        assert response.status_code == 200
-
-    def test_authenticated(self, client):
-        """An authenticated user gets a valid response."""
-        client.force_login(UserFactory())
-
         response = client.get(reverse("index"))
 
         assert response.status_code == 200
