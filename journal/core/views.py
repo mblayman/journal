e8234829426566e3b96487871d663be5e06eec13
Matt Layman <matthewlayman@gmail.com>
2025-03-23 15:16:31 -0400

    Remove more code.

--- journal/core/views.py
+++ journal/core/views.py
@@ -2,19 +2,12 @@ from denied.decorators import allow
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from journal.accounts import constants
-
 
 @allow
 def index(request: HttpRequest) -> HttpResponse:
     """The entry point for the website."""
-    context: dict = {"trial_days": constants.TRIAL_DAYS}
-    template_name = "core/index_unauthenticated.html"
-
-    if request.user.is_authenticated:
-        template_name = "core/index.html"
-
-    return render(request, template_name, context)
+    template_name = "core/index.html"
+    return render(request, template_name, {})
 
 
 @allow
